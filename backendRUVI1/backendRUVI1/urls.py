1555467ce3216cc929e8c74e38204519246f1318
Jennifer <jennifer.velozo@usach.cl>
2022-10-11 19:43:32 -0300

    Intento de registro

--- backendRUVI1/backendRUVI1/urls.py
+++ backendRUVI1/backendRUVI1/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from gestionPacientes.views import  UsuarioViewSet, usuarios
+
+
+from rest_framework.routers import DefaultRouter
+from gestionPacientes.views import UsuarioSerializer
+router = DefaultRouter()
+router.register(r'usuarios', UsuarioViewSet)
+urlpatterns = router.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('usuarios/', usuarios)
 ]
--- backendRUVI1/gestionPacientes/views.py
+++ backendRUVI1/gestionPacientes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.core import serializers
+from gestionPacientes.models import Usuarios
+
+from .serializers import UsuarioSerializer
+from django.http import HttpResponse
 
 # Create your views here.
+def usuarios(request):
+    data = Usuarios.objects.all()
+    #print(data)
+
+    data1 = serializers.serialize('json', data)
+    print(data1)
+
+
+    return HttpResponse(data1, content_type='application/json')
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    serializer_class = UsuarioSerializer
+    queryset = Usuarios.objects.all()
+    print(queryset)
