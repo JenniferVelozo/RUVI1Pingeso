68251aa3def2d2b1ac21bee293455dbee0b83725
maximiliano araya <maximiliano.araya@usach.cl>
2022-10-11 21:19:14 -0300

    loaddata

---
+++ backendRUVI1/gestionPacientes/loadCSV.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+from numpy import arange
+from sqlalchemy import create_engine
+import sqlalchemy as sqlalchemy
+from gestionPacientes.dbConnection import conectar_db
+
+
+def load_CIE10_GRD(archivo):
+    conn = conectar_db()
+    print(conn)
+    cie10 = pd.read_excel(archivo, sheet_name='CIE10 MOD')
+    listaID = arange(1,cie10.shape[0]+1,1).tolist()
+    cie10 = cie10.assign(id=listaID)
+    print(cie10)
+    cie10.to_sql("cie10", con=conn, if_exists="replace", index=False, dtype={'id': sqlalchemy.types.BigInteger()})
+    conn.execute('ALTER TABLE cie10 ADD PRIMARY KEY (id);')
+
+    norma = pd.read_excel(archivo, sheet_name='NORMA')
+    listaID = arange(1,norma.shape[0]+1,1).tolist()
+    norma = norma.assign(id=listaID)
+    print(norma)
+    norma.to_sql("norma", con=conn, if_exists="replace", index=False, dtype={'id': sqlalchemy.types.BigInteger()})
+    conn.execute('ALTER TABLE norma ADD PRIMARY KEY (id);')
+    conn.dispose()
+
+
+def load_prestaciones(archivo):
+    conn = conectar_db()
+    prestaciones = pd.read_excel(archivo, sheet_name='Prestaciones')
+    listaID = arange(1,prestaciones.shape[0]+1,1).tolist()
+    prestaciones = prestaciones.assign(id=listaID)
+    print(prestaciones)
+    prestaciones.to_sql("pendiente", con=conn, if_exists="replace", index=False, dtype={'id': sqlalchemy.types.BigInteger()})
+    conn.execute('ALTER TABLE pendiente ADD PRIMARY KEY (id);')
+    conn.dispose()
+
+
+def load_inicial():
+    print("Aquí leemos los archivos CIE 10 y Norma y los almacenamos en la DB")
+    path = os.path.dirname(os.path.realpath(__file__))
+    archivo = path+'\CIE10-GRD.xlsm'
+    print(archivo)
+    load_CIE10_GRD(archivo)
+
+    path = os.path.dirname(os.path.realpath(__file__))
+    archivo = path+'\PRESTACIONES_CAUSAS.xlsx'
+    print(archivo)
+    load_prestaciones(archivo)
+
+if __name__=='__main__':
+    load_inicial()
--- backendRUVI1/manage.py
+++ backendRUVI1/manage.py
@@ -8,41 +8,8 @@ from numpy import arange
 from sqlalchemy import create_engine
 import sqlalchemy as sqlalchemy
 
-def conectar_db(username, password, host, port, database):
-    conn = 'postgresql://'+ username +':'+ password + '@' + host +':'+ port+'/'+ database
-    # 'postgresql://postgres:postgres@admin:5432/RUVI1'
-    # Conexión a base de datos local
-    engine = create_engine(conn)
-    return engine
 
 def main():
-    print("Aquí leemos los archivos CIE 10 y Norma y los almacenamos en la DB")
-    path = os.path.dirname(os.path.realpath(__file__))
-
-    conn = conectar_db('postgres', 'postgres', '127.0.0.1', '5432', 'RUVI1')
-    print(conn)
-
-    archivo = path+'\CIE10-GRD.xlsm'
-    #print(archivo)
-
-    cie10 = pd.read_excel(archivo, sheet_name='CIE10 MOD')
-    listaID = arange(1,cie10.shape[0]+1,1).tolist()
-    cie10 = cie10.assign(id=listaID)
-    print(cie10)
-    cie10.to_sql("cie10", con=conn, if_exists="replace", index=False, dtype={'id': sqlalchemy.types.BigInteger()})
-    conn.execute('ALTER TABLE cie10 ADD PRIMARY KEY (id);')
-
-    norma = pd.read_excel(archivo, sheet_name='NORMA')
-    listaID = arange(1,norma.shape[0]+1,1).tolist()
-    norma = norma.assign(id=listaID)
-    print(norma)
-    norma.to_sql("norma", con=conn, if_exists="replace", index=False)
-    conn.execute('ALTER TABLE norma ADD PRIMARY KEY (id);')
-    
-
-    
-
-
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendRUVI1.settings')
     try:
