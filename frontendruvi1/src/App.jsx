09b01f1a6ca32d71f126ac312ba1a7c0aa86635e
Jennifer <jennifer.velozo@usach.cl>
2022-10-16 15:18:21 -0300

    Prueba de Login y Registro funcionando

--- frontendruvi1/src/App.jsx
+++ frontendruvi1/src/App.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 
-import Login from './components/login';
-import Home from './components/home';
-import Register from './components/register';
-import Resumen from './components/resumen';
-import Lala from './components/lala';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import BasicSelect from './components/lala';
+//import Login from './components/login';
+//import Home from './components/home';
+//import Register from './components/register';
+//import Resumen from './components/resumen';
+//import Lala from './components/lala';
+//import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+//import BasicSelect from './components/lala';
+
+
+/* Intento de login */
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginReg from "./pages/auth/LoginReg";
+import ResetPassword from "./pages/auth/ResetPassword";
+import SendPasswordResetEmail from "./pages/auth/SendPasswordResetEmail";
+
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import { useSelector } from "react-redux";
 
 
 function App() {
+  const { access_token } = useSelector(state => state.auth)
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={!access_token ? <LoginReg /> : <Navigate to="/dashboard" />} />
+            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
+            <Route path="api/user/reset/:id/:token" element={<ResetPassword />} />
+          </Route>
+          <Route path="/dashboard" element={access_token ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+
+/*
+----- LO QUE ESTABA ANTES ----------- 
+function App() {
+  
   return (
     <Router>
         <div>
@@ -19,10 +55,11 @@ function App() {
               <Route path='/register' element={<Register/>} />
               <Route path='/resumen' element={<Resumen/>} />
               <Route path='/lala' element={<BasicSelect/>} />
+              
           </Routes>
         </div>
       </Router>
   );
-}
+}*/
 
 export default App;
--- frontendruvi1/src/index.js
+++ frontendruvi1/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+//import App from './App.jsx';
 
+/* INTENTO DE LOGIN */
+
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+
+
+
+/*
+LO QUE ESTABA ANTES
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
